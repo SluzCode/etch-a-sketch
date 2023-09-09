97fc488e43cd03ae99841a16e988af339821fe6b
SluzCode <17539585+SluzCode@users.noreply.github.com>
2023-09-09 12:26:20 -0700

    Store code for basic 10x10 grid with css hover
    This is the basic 10 x 10 grid created by javascript with css
    hover effect implemented. Now I need to do the event listener aspect
    of the project, among other things like adding the button and
    turning the tile generator into a function.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-const container = document.querySelector('#container');
+/* Pseudocode for 
 
-const div = document.createElement('div');
-// creates a new div referenced in the variable 'div'
+assume 5 x 5 grid
 
-/*
+create 5 divs for each row
 
-create 
+  create 5 child items for each row giving you the 5x5 grid
+  
 
 
 
@@ -20,124 +20,46 @@ create
 
 
 // Etch a sketch project here
+// const container = document.querySelector('#container');
+
+// const div = document.createElement('div');
+// creates a new div referenced in the variable 'div'
 
 const easContainer = document.querySelector('#easContainer');
 
 
-// let numOfWindows = 3;
+let numOfRows = 10;
 
-// let arrayDiv = new Array();
-// for (let i = 0; i < numOfWindows; i++) {
-//     arrayDiv[i] = document.createElement('div');
-//     arrayDiv[i].id = 'block' + i;
-//     arrayDiv[i].style.backgroundColor = 'green';
-//     arrayDiv[i].className = 'block' + i;
+let arrayDiv = new Array();
+for (let i = 0; i < numOfRows; i++) {
+  arrayDiv[i] = document.createElement('div');
+  arrayDiv[i].id = 'row' + i;
+  // arrayDiv[i].style.backgroundColor = 'white';
+  arrayDiv[i].className = 'row' + i;
 
-//     arrayDiv[i].className = 'bordered';
+  arrayDiv[i].className = 'rowStyle';
 
-//     document.body.appendChild(arrayDiv[i]);
-// }
+  document.body.appendChild(arrayDiv[i]);
+}
 
+let numOfColumns = 10;
 
+let arrayDivChild = new Array();
+for (let j = 0; j < numOfRows; j++) {
+  for (let i = 0; i < numOfColumns; i++) {
+    arrayDivChild[i] = document.createElement('div');
+    arrayDivChild[i].id = 'column' + j;
+    // arrayDivChild[i].style.backgroundColor = 'orange';
+    arrayDivChild[i].className = 'column' + i;
 
+    arrayDivChild[i].className = 'tileStyle';
 
+    // arrayDivChild[i].className = 'columnStyle';
 
-// End of Etch a sketch code
+    document.querySelector(`#row${i}`).appendChild(arrayDivChild[i]);
 
+    // console.log(document.querySelector(`#row${i}`));
+  }
+}
 
-
-// div.style.color = 'blue';
-// adds the indicated style rule
-
-// div.style.border = "5px solid white"
-
-div.style.cssText = 'color:black; background: pink; border: 5px dotted green';
-// adds several style rules
-
-// div.setAttribute('style', 'color: blue; background: lightgray;');
-// adds several style rules
-
-// div.textContent = 'This is the glorious text-content!'                               
-// creates a text node containing "Hello World!" and
-// inserts it in div
-
-const header1 = document.createElement('h1');
-
-div.appendChild(header1);
-
-header1.textContent = "I'm the h1 element!"
-
-const innerPara = document.createElement('p');
-
-innerPara.textContent = "I'm the <p> that comes after";
-
-div.appendChild(innerPara)
-
-container.appendChild(div);
-
-// Exercises
-
-
-// const para = document.createElement('p');
-
-// para.textContent = "Hey I'm Red";
-
-// para.style.color = 'red'
-
-// container.appendChild(para);
-
-
-// const header3 = document.createElement('h1');
-
-// header3.textContent = "Hey I'm Red";
-
-// header3.style.color = 'blue'
-
-// container.appendChild(header3);
-
-
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
-
-// the JavaScript file
-
-
-
-
-const btn2 = document.querySelector('#btn2');
-
-
-btn2.addEventListener('click', function (e) {
-    console.log(e);
-  });
-
-
-
-btn2.addEventListener('click', function (e) {
-    console.log(e.target);
-  });
-
-  btn2.addEventListener('click', function (e) {
-    e.target.style.background = 'lightblue';
-  });
-
-
-  // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.id);
-  });
-
-  button.addEventListener('click', function (e) {
-    console.log(e.target);
-  });
-
-  button.addEventListener('click', function (e) {
-    e.target.style.background = 'lightblue';
-  });
-});
+// const nodeList = document.body.childNodes;
