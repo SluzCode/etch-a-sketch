8853e7eb0600212876297f8bf50d80e30b13ede1
SluzCode <17539585+SluzCode@users.noreply.github.com>
2023-09-12 12:25:36 -0700

    Allow fast transition in, hold, and slow transition out
    Added a mouse out listener and tweaked remove transition function
    function to allow change to remain until mouseout event.

--- scripts.js
+++ scripts.js
@@ -28,77 +28,106 @@ create 5 divs for each row
 const easContainer = document.querySelector('#easContainer');
 
 
-function generateGrid(numOfRows,numOfColumns){
+function generateGrid(numOfRows, numOfColumns) {
 
-// let numOfRows = 10;
+  // let numOfRows = 10;
 
-easContainer.innerHTML = "";
+  easContainer.innerHTML = "";
 
-let arrayDiv = new Array();
-for (let i = 0; i < numOfRows; i++) {
-  arrayDiv[i] = document.createElement('div');
-  arrayDiv[i].id = 'row' + i;
-  // arrayDiv[i].style.backgroundColor = 'white';
-  arrayDiv[i].className = 'row' + i;
+  let arrayDiv = new Array();
+  for (let i = 0; i < numOfRows; i++) {
+    arrayDiv[i] = document.createElement('div');
+    arrayDiv[i].id = 'row' + i;
+    // arrayDiv[i].style.backgroundColor = 'white';
+    arrayDiv[i].className = 'row' + i;
 
-  arrayDiv[i].className = 'rowStyle';
+    arrayDiv[i].className = 'rowStyle';
 
-  easContainer.appendChild(arrayDiv[i]);
-}
-
-// let numOfColumns = 10;
-
-let arrayDivChild = new Array();
-for (let j = 0; j < numOfRows; j++) {
-  for (let i = 0; i < numOfColumns; i++) {
-    arrayDivChild[i] = document.createElement('div');
-    arrayDivChild[i].id = 'row' + i + 'column' + j;
-    // arrayDivChild[i].style.backgroundColor = 'orange';
-    arrayDivChild[i].className = 'column' + i;
-
-    arrayDivChild[i].className = 'tile';
-
-    // arrayDivChild[i].className = 'columnStyle';
-
-    document.querySelector(`#row${i}`).appendChild(arrayDivChild[i]);
-
-    // console.log(document.querySelector(`#row${i}`));
+    easContainer.appendChild(arrayDiv[i]);
   }
-}
-addListener()
-// return easContainer;
+
+  // let numOfColumns = 10;
+
+  let arrayDivChild = new Array();
+  for (let j = 0; j < numOfRows; j++) {
+    for (let i = 0; i < numOfColumns; i++) {
+      arrayDivChild[i] = document.createElement('div');
+      arrayDivChild[i].id = 'row' + i + 'column' + j;
+      // arrayDivChild[i].style.backgroundColor = 'orange';
+      arrayDivChild[i].className = 'column' + i;
+
+      arrayDivChild[i].className = 'tile';
+
+      // arrayDivChild[i].className = 'columnStyle';
+
+      document.querySelector(`#row${i}`).appendChild(arrayDivChild[i]);
+
+      // console.log(document.querySelector(`#row${i}`));
+    }
+  }
+  addListener()
+  // return easContainer;
 }
 
-generateGrid(10,10);
+generateGrid(16, 16);
 // const nodeList = document.body.childNodes;
 
-function addListener(){
+function addListener() {
 
 
-const tiles = document.querySelectorAll('.tile');
+  const tiles = document.querySelectorAll('.tile');
 
-tiles.forEach(tile => tile.addEventListener(
-  "mouseover",
-  (event) => {
-    // highlight the mouseover target
-    // console.log('hovered')
-    tile.classList.add('hovered');
-    // console.log(tile);
-  },
-  false,
-));
+  tiles.forEach(tile => tile.addEventListener(
+    "mouseover",
+    (event) => {
+      // highlight the mouseover target
+      // console.log('hovered')
+      tile.classList.remove('fadeout')
+      tile.classList.add('hovered');
+      // console.log(tile);
+    },
+    false,
+  ));
 
-tiles.forEach(tile => tile.addEventListener('transitionend', removeTransition));
+  tiles.forEach(tile => tile.addEventListener(
+    "mouseout",
+    (event) => {
+      // highlight the mouseover target
+      console.log('mouse out')
+      tile.classList.remove('hovered');
+      tile.classList.add('fadeout');
+      // console.log(tile);
+    },
+    false,
+  ));
 
-function removeTransition(e){
-  if(e.propertyName !== 'background-color') return; 
-  this.classList.remove('hovered');
-  // console.log(this);
-}
-return tiles;
+  // tiles.forEach(tile => tile.addEventListener('transitionend', removeTransition));
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'background-color') return;
+    this.classList.remove('hovered');
+    this.classList.remove('fadeout');
+    // console.log(this.classList);
+  }
+  return tiles;
 }
 // const keys = document.querySelectorAll('.key');
 // keys.forEach(key => key.addEventListener('transitionend',removeTransition));
 
 
 // window.addEventListener('keydown',playSound)
+
+
+function getValue() {
+  var sliderValue = document.getElementById("mySlider").value;
+  alert("Slider Value: " + sliderValue);
+}
+
+var slider = document.getElementById("mySlider");
+var output = document.getElementById("sliderValue");
+//   output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  console.log(this.value);
+  output.innerHTML = this.value;
+}
